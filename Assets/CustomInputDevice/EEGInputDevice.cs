6a036398e11ed844f69b3dd929e32f1a091ccad9
mms6224 <maxmsomarriba@utexas.edu>
2024-12-01 17:01:31 -0600

    better eeg input device

--- Assets/CustomInputDevice/EEGInputDevice.cs
+++ Assets/CustomInputDevice/EEGInputDevice.cs
@@ -1,48 +1,63 @@
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Controls;
 using UnityEngine.InputSystem.Layouts;
+using UnityEngine.InputSystem.LowLevel;
+using UnityEngine.InputSystem.Utilities;
 
-// Added to auto run static constructor after C# domain load
-#if UNITY_EDITOR
+
+public struct EEGInputState : IInputStateTypeInfo
+{
+    public FourCC format => new FourCC('E', 'E', 'G', 'I');
+    [InputControl(name = "forwardButtonInput", layout = "Button", bit = 0)]
+    [InputControl(name = "backwardButtonInput", layout = "Button", bit = 1)]
+    public int buttons;
+}
+
+[InputControlLayout(displayName = "EEG Input Device", stateType = typeof(EEGInputState))]
 [InitializeOnLoad]
-#endif
-[InputControlLayout(displayName = "EEG Input Device")]
 public class EEGInputDevice : InputDevice
 {
-    public ButtonControl button1 {get; private set;}
-    public ButtonControl button2 {get; private set;}
-    public ButtonControl button3 {get; private set;}
-    public ButtonControl button4 {get; private set;}
-    public ButtonControl button5 {get; private set;}
-    public ButtonControl button6 {get; private set;}
+    public ButtonControl forwardButtonInput {get; private set;}
+    public ButtonControl backwardButtonInput {get; private set;}
 
-    static EEGInputDevice(){
-        // Adds control layout to the the input system
-        InputSystem.RegisterLayout<EEGInputDevice>();
-    }
+    // static EEGInputDevice(){
+    //     // Adds control layout to the the input system
+    //     InputSystem.RegisterLayout<EEGInputDevice>();
+    // }
 
     // Apparently this triggers static constructor of our input device in the player
-    [RuntimeInitializeOnLoadMethod]
-    private static void InitializeInPlayer() {}
+    // [RuntimeInitializeOnLoadMethod]
+    // private static void InitializeInPlayer() {}
 
     // Input System call this after it constructs the device and before it adds the device to system
     // Any last setup can be added here
     protected override void FinishSetup(){
         base.FinishSetup();
-
-        button1 = GetChildControl<ButtonControl>("button1");
-        button2 = GetChildControl<ButtonControl>("button2");
-        button3 = GetChildControl<ButtonControl>("button3");
-        button4 = GetChildControl<ButtonControl>("button4");
-        button5 = GetChildControl<ButtonControl>("button5");
-        button6 = GetChildControl<ButtonControl>("button6");
+        forwardButtonInput = GetChildControl<ButtonControl>("forwardButtonInput");
+        backwardButtonInput = GetChildControl<ButtonControl>("backwardButtonInput");
+    //     // button3 = GetChildControl<ButtonControl>("button3");
+    //     // button4 = GetChildControl<ButtonControl>("button4");
+    //     // button5 = GetChildControl<ButtonControl>("button5");
+    //     // button6 = GetChildControl<ButtonControl>("button6");
     }
 
-    [MenuItem("Tools/Add EEG Input Device")]
+    static EEGInputDevice(){
+        InputSystem.RegisterLayout<EEGInputDevice>();
+
+        if(!InputSystem.devices.Any(x => x is EEGInputDevice)){
+            InputSystem.AddDevice<EEGInputDevice>();
+        }
+        
+    }
+
+    [RuntimeInitializeOnLoadMethod]
     public static void Initialize(){
         // This is called by the Input System to initialize the device
-        InputSystem.AddDevice<EEGInputDevice>();
+        // var eegInputDevice = InputSystem.AddDevice<EEGInputDevice>();
+        // InputSystem.QueueStateEvent(eegInputDevice, new EEGInputState{buttons = 0});
+        
     }
 }
